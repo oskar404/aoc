52fecab168d3da42656cfad87e193ec558f34c54
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2019-12-01 19:35:17 +0200

    Learning ..
    Always learning something. There is an operator // in Python which
    does same as math.floor(n/m).

--- day01.py
+++ day01.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-from math import floor
 
 
 # First 'simple' solution
@@ -10,7 +9,7 @@ def solve_part1(file):
     total = 0
     with open(file) as f:
         for line in f:
-            total += floor(int(line)/3)-2
+            total += int(line)//3-2
     return total
 
 
@@ -18,7 +17,7 @@ def fuel_requirement(mass):
     total = 0
     fuel = mass
     while True:
-        fuel = floor(fuel/3)-2
+        fuel = fuel//3-2
         if fuel <= 0:
             break
         total += fuel
@@ -45,7 +44,7 @@ def read_data(file):
         return [int(line) for line in f]
 
 data = read_data(sys.argv[1])
-result1 = sum([floor(i/3)-2 for i in data])
+result1 = sum([i//3-2 for i in data])
 print(f"Fuel requirements (for modules): {result1}")
 result2 = sum([fuel_requirement(i) for i in data])
 print(f"Fuel requirements (for modules+fuel): {result2}")
