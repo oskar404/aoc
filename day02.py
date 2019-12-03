94cb512660bedcff23941eb7810a127fa2653453
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2019-12-03 20:48:58 +0200

    Add part2 solution for day02
    Nice little puzzle all in all

--- day02.py
+++ day02.py
@@ -8,8 +8,11 @@ def read_data(file):
         return [int(i) for i in f.read().split(',')]
 
 
-def parse(prog):
-    """Parse the program data"""
+def processor(data, noun, verb):
+    """Load the program into this processor, set the inputs and run the program
+
+    :returns: result
+    """
 
     def add(idx):
         """Addition operator"""
@@ -30,19 +33,37 @@ def parse(prog):
         2: mul,
         99: halt
     }
+
+    # Load program and set inputs
+    prog = data.copy()
+    prog[1] = noun
+    prog[2] = verb
+
+    # Run the program
     idx = 0
-    while True:
-        opcode = operands.get(prog[idx])
-        assert opcode, f"operation failed at {idx} with opcode: {opcode}"
-        if opcode(idx):
-            idx += 4
-        else:
-            return prog
+    try:
+        while True:
+            opcode = operands.get(prog[idx])
+            assert opcode, f"operation failed at {idx} with opcode: {opcode}"
+            if opcode(idx):
+                idx += 4
+            else:
+                return prog[0]
+    except IndexError:
+        assert False, f"Invalid input: {noun} {verb}"
+
+
+def search(data, result):
+    """Iterate over the search space for correct input"""
+    for noun in range(100):
+        for verb in range(100):
+            output = processor(data, noun, verb)
+            if output == result:
+                return (noun, verb)
 
 
 assert len(sys.argv) == 2, "Missing input"
 data = read_data(sys.argv[1])
-program = data.copy()  # Keep original data intact
-program[1] = 12
-program[2] = 2
-print(f"1202 program alarm state: {parse(program)[0]}")
+print(f"1202 program alarm state: {processor(data, 12, 2)}")
+noun, verb = search(data, 19690720)
+print(f"Gravity assist 19690720 ({noun},{verb}) -> {100 * noun + verb} ")
