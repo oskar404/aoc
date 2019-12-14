4217610723687728b3d0b0e026c329a9c26bcb1b
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2019-12-14 21:40:36 +0200

    Add solution for day07 part2
    Lot of went into refactoring the processor and debugging some
    stupid mistake (wrong function call)

--- day07.py
+++ day07.py
@@ -12,115 +12,180 @@ def read_data(file):
 class AmpIo:
     """Amp IO Interface implementation"""
 
-    def __init__(self, phase, input):
+    def __init__(self, phase, input=None):
+        self._odx = 0
         self.stdout = []
-        self.ioidx = -1
-        self.stdin = [phase, input]
+        self._idx = 0
+        self.stdin = [phase]
+        if input != None:
+            self.stdin.append(input)
 
     def input(self):
-        self.ioidx += 1
-        return self.stdin[self.ioidx]
+        self._idx += 1
+        return self.stdin[self._idx-1]
+
+    def has_input(self):
+        return self._idx < len(self.stdin)
+
+    def add_input(self, value):
+        self.stdin.append(value)
 
     def output(self, value):
         self.stdout.append(value)
 
+    def read_output(self):
+        self._odx += 1
+        return self.stdout[self._odx-1]
+
     def __str__(self):
-        return f"stdin:{self.stdin} stdout:{self.stdout}"
+        return f"stdin:{self.stdin} [{self._idx}] stdout:{self.stdout} [{self._odx}]"
 
     def __repr__(self):
-        return f"stdin:{self.stdin} stdout:{self.stdout}"
+        return f"stdin:{self.stdin} [{self._idx}] stdout:{self.stdout} [{self._odx}]"
 
 
-def processor(data, io, debug=False):
-    """Load the program into this processor, set the inputs and run the program
+class ProcessorState:
+    """IntCode processor state.
 
-    :returns: result
-    """
+    Load the program into this processor, set the inputs and run the program"""
 
-    def parse_opcode(code):
+    def __init__(self, data):
+        self.prog = data.copy()  # Processor byte code
+        self.idx = 0             # Current instruction
+        self._halt = False       # Processor in halt state
+
+    def read(self, offset=0):
+        """Return value from memory"""
+        return self.prog[self.idx + offset]
+
+    def writ(self, value, offset=0):
+        """Write value to memory"""
+        self.prog[self.prog[self.idx + offset]] = value
+
+    def next(self, offset=0):
+        """Progress to next instruction"""
+        self.idx += offset
+
+    def jump(self, ptr):
+        """Jump to instruction"""
+        self.idx = ptr
+
+    def halt(self):
+        """Return true if halted"""
+        return self._halt
+
+    def hlcf(self):
+        """Halt and catch fire"""
+        self._halt = True
+
+    def inst(self):
         """Return opcode and modes"""
+        code = self.read()
         opcode = code % 100
         modes = [int(code/100)%10, int(code/1000)%10, int(code/10000)%10]
         return opcode, modes
 
-    def readreg(idx, mode):
+    def parm(self, offset, mode):
         """Read parameter based on mode"""
-        if mode == 0:
-            # position mode
-            addr = prog[idx]
-            assert addr >=0, f"Invalid addr at {idx}: {addr}"
-            return prog[addr]
-        else:
-            # immediate mode
-            return prog[idx]
+        ptr = self.idx + offset
+        value = self.prog[ptr]
+        if mode == 0:   # position mode
+            return self.prog[value]
+        return value    # immediate mode
 
-    def add(idx, modes):
+
+def null_debug(state, cmd, args, result, modes):
+    """Null debugger, no output"""
+    pass
+
+
+def debugger(state, cmd, modes, args=0, result=None):
+    """Dump processor instructions to stdout"""
+    arglist = []
+    for i in range(args):
+        arglist.append(state.read(i+1))
+    res =  state.read(args+1) if result else '<undef>'
+    print(f"{cmd}({arglist}) -> {res} [idx:{state.idx},modes:{modes}]")
+
+
+def processor(state, io, debug=null_debug):
+    """IntCode processor.
+
+    Load the program into this processor, set the inputs and run the program"""
+
+    def add(modes):
         """Addition operator: 01, arg, arg, result"""
-        if debug:
-            print(f"add({prog[idx+1]},{prog[idx+2]}) -> {prog[idx+3]} [idx:{idx},modes:{modes}]")
-        prog[prog[idx+3]] = readreg(idx+1, modes[0]) + readreg(idx+2, modes[1])
-        return idx+4
+        debug(state, 'add', modes, 2, True)
+        state.writ(state.parm(1, modes[0]) + state.parm(2, modes[1]), 3)
+        state.next(4)
+        return True
 
-    def multiply(idx, modes):
+    def multiply(modes):
         """Multiplication operator: 02, arg, arg, result*"""
-        if debug:
-            print(f"multiply({prog[idx+1]},{prog[idx+2]}) -> {prog[idx+3]} [idx:{idx},modes:{modes}]")
-        prog[prog[idx+3]] = readreg(idx+1, modes[0]) * readreg(idx+2, modes[1])
-        return idx+4
+        debug(state, 'multiply', modes, 2, True)
+        state.writ(state.parm(1, modes[0]) * state.parm(2, modes[1]), 3)
+        state.next(4)
+        return True
 
-    def read_input(idx, modes):
+    def read_input(modes):
         """Take input operator: 03, arg <- read input to arg"""
-        if debug:
-            print(f"read_input() -> {prog[idx+1]} [idx:{idx},modes:{modes}]")
-        prog[prog[idx+1]] = io.input()
-        return idx+2
+        debug(state, 'read_input', modes, 0, True)
+        if io.has_input():
+            state.writ(io.input(), 1)
+            state.next(2)
+            return True
+        return False
 
-    def write_output(idx, modes):
+    def write_output(modes):
         """Output operator: 04, arg -> write arg to output output"""
-        if debug:
-            print(f"write_output({prog[idx+1]}) [idx:{idx},modes:{modes}]")
-        io.output(readreg(idx+1, modes[0]))
-        return idx+2
+        debug(state, 'write_output', modes, 1, False)
+        io.output(state.parm(1, modes[0]))
+        state.next(2)
+        return True
 
-    def jmp_if_true(idx, modes):
+    def jmp_if_true(modes):
         """Jump-if-true operator: 05, arg, result"""
-        if debug:
-            print(f"jmp_if_true({prog[idx+1]}) -> {prog[idx+2]} [idx:{idx},modes:{modes}]")
-        if readreg(idx+1, modes[0]) != 0:
-            return readreg(idx+2, modes[1])
-        return idx+3
+        debug(state, 'jmp_if_true', modes, 1, True)
+        if state.parm(1, modes[0]) != 0:
+            state.jump(state.parm(2, modes[1]))
+        else:
+            state.next(3)
+        return True
 
-    def jmp_if_false(idx, modes):
+    def jmp_if_false(modes):
         """Jump-if-false operator: 06, arg, result"""
-        if debug:
-            print(f"jmp_if_false({prog[idx+1]}) -> {prog[idx+2]} [idx:{idx},modes:{modes}]")
-        if readreg(idx+1, modes[0]) == 0:
-            return readreg(idx+2, modes[1])
-        return idx+3
+        debug(state, 'jmp_if_false', modes, 1, True)
+        if state.parm(1, modes[0]) == 0:
+            state.jump(state.parm(2, modes[1]))
+        else:
+            state.next(3)
+        return True
 
-    def less_than(idx, modes):
+    def less_than(modes):
         """Less-than operator: 07, arg, arg, result"""
-        if debug:
-            print(f"less_than({prog[idx+1]},{prog[idx+2]}) -> {prog[idx+3]} [idx:{idx},modes:{modes}]")
-        if readreg(idx+1, modes[0]) < readreg(idx+2, modes[1]):
-            prog[prog[idx+3]] = 1
+        debug(state, 'less_than', modes, 2, True)
+        if state.parm(1, modes[0]) < state.parm(2, modes[1]):
+            state.writ(1, 3)
         else:
-            prog[prog[idx+3]] = 0
-        return idx+4
+            state.writ(0, 3)
+        state.next(4)
+        return True
 
-    def equals(idx, modes):
+    def equals(modes):
         """Less-than operator: 08, arg, arg, result"""
-        if debug:
-            print(f"equals({prog[idx+1]},{prog[idx+2]}) -> {prog[idx+3]} [idx:{idx},modes:{modes}]")
-        if readreg(idx+1, modes[0]) == readreg(idx+2, modes[1]):
-            prog[prog[idx+3]] = 1
+        debug(state, 'equals', modes, 2, True)
+        if state.parm(1, modes[0]) == state.parm(2, modes[1]):
+            state.writ(1, 3)
         else:
-            prog[prog[idx+3]] = 0
-        return idx+4
+            state.writ(0, 3)
+        state.next(4)
+        return True
 
-    def halt(idx, modes):
+    def halt(modes):
         """Halt operator: 99"""
-        return -1
+        debug(state, 'halt', modes, 0, False)
+        state.hlcf()
+        return False
 
     operands = {
         1: add,
@@ -134,35 +199,32 @@ def processor(data, io, debug=False):
         99: halt
     }
 
-    # Load program
-    prog = data.copy()
-
-    # Run the program
-    idx = 0
+    assert not state.halt()
     try:
         while True:
-            code, modes = parse_opcode(prog[idx])
+            code, modes = state.inst()
             opcode = operands.get(code)
-            assert opcode, f"operation failed at {idx} opcode: {prog[idx]}"
-            idx = opcode(idx, modes)
-            if idx < 0:
-                return prog
+            assert opcode, f"operation failed at {state.idx} -> {state.read()}"
+            if not opcode(modes):
+                return state.halt()
+
     except IndexError:
-        assert False, f"Invalid index: {idx}"
+        assert False, f"Invalid index: {state.idx}"
 
 
-def  amplify(data, phases):
+def amplify(data, phases):
     register = 0
     for phase in phases:
         io = AmpIo(phase, register)
-        processor(data, io)
+        state = ProcessorState(data)
+        processor(state, io)
         assert len(io.stdout) == 1
         register = io.stdout[0]
     return register
 
 
 def max_thrust(data):
-    """Search max thruster configuration"""
+    """Search max thrust configuration (solution for part1)"""
     permutations = list(itertools.permutations([0, 1, 2, 3, 4]))
     max_value = -1
     phase_config = []
@@ -174,13 +236,59 @@ def max_thrust(data):
     return max_value, phase_config
 
 
+def amp_feebback_loop(data, phases):
+    thruster = 0
+    input = 0
+    amps = []
+    for phase in phases:
+        amps.append((ProcessorState(data), AmpIo(phase)))
+    while True:
+        halt = False
+        for state, io in amps:
+            io.add_input(input)
+            halt = processor(state, io)
+            input = io.read_output()
+        thruster = input
+        if halt:
+            break
+    return thruster
+
+
+def more_thrust(data):
+    """Search max thrust config with feedback loop (solution for part2)"""
+    permutations = list(itertools.permutations([5, 6, 7, 8, 9]))
+    max_value = -1
+    phase_config = []
+    for config in permutations:
+        thrust = amp_feebback_loop(data, config)
+        if thrust > max_value:
+            max_value = thrust
+            phase_config = config
+    return max_value, phase_config
+
+
 def self_test():
+    # part1 tests
     thrust, phase = max_thrust([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
-    assert thrust == 43210 and phase == (4,3,2,1,0)
-    thrust, phase = max_thrust([3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0])
-    assert thrust == 54321 and phase == (0,1,2,3,4)
-    thrust, phase = max_thrust([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0])
-    assert thrust == 65210 and phase == (1,0,4,3,2)
+    assert thrust == 43210 and phase == (4,3,2,1,0), f"{thrust}, {phase}"
+    thrust, phase = max_thrust([
+        3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,
+        99,0,0])
+    assert thrust == 54321 and phase == (0,1,2,3,4), f"{thrust}, {phase}"
+    thrust, phase = max_thrust([
+        3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,
+        31,31,1,32,31,31,4,31,99,0,0,0])
+    assert thrust == 65210 and phase == (1,0,4,3,2), f"{thrust}, {phase}"
+    # part2 tests
+    thrust, phase = more_thrust([
+        3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+        27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5])
+    assert thrust == 139629729 and phase == (9,8,7,6,5), f"{thrust}, {phase}"
+    thrust, phase = more_thrust([
+            3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+            -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+            53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10])
+    assert thrust == 18216 and phase == (9,7,8,5,6), f"{thrust}, {phase}"
 
 
 assert len(sys.argv) == 2, "Missing input"
@@ -191,3 +299,5 @@ else:
     data = read_data(sys.argv[1])
     thrust, phase = max_thrust(data)
     print(f"Max thrust: {thrust} {phase}")
+    thrust, phase = more_thrust(data)
+    print(f"More thrust: {thrust} {phase}")
