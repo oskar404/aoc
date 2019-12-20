6afd4a62fbc0e5c1005853d800691dcdb9f8b4fb
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2019-12-21 00:08:14 +0200

    Refactor IntCode computer
    Refactor and remove some memory address handling bugs.

--- intcode.py
+++ intcode.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
 """IntCode computer
 
-The IntCode computer uses list of integers as instruction set"""
+The IntCode computer uses list of integers as instruction set:
+
+Opcode 1:   add(p1, p2) -> p3
+Opcode 2:   multiply(p1, p2) -> p3
+Opcode 3:   read_input() -> p1
+Opcode 4:   write_output(p1)
+Opcode 5:   jump_if_true(p1 != 0) -> p2
+Opcode 6:   jump_if_false(p1 == 0) -> p2
+Opcode 7:   less_than(p1, p2) -> p3
+Opcode 8:   equals(p1, p2) -> p3
+Opcode 9:   base_offset(p1)
+Opcode 99:  halt
+
+Opcode parameter mode calculation:
+
+    zyxCC
+
+    CC  -> Opcode
+    x   -> mode for p1
+    y   -> mode for p2
+    z   -> mode for p3
+
+Values for modes:
+
+    0   -> position mode
+    1   -> immediate mode
+    2   -> relative mode
+"""
 
 
 class IntCodeIO:
@@ -47,13 +74,12 @@ class IntCodeState:
 
     Returns True if halted. If False waiting for input."""
 
-    def __init__(self, data, debug=False):
+    def __init__(self, data):
         """Load program into this state"""
         self.prog = data.copy()  # Processor byte code
         self._idx = 0            # Current instruction
         self._halt = False       # Processor in halt state
-        self._relative_base = 0
-        self._dbg = debug
+        self._base = 0           # Relative base
 
     @property
     def idx(self):
@@ -63,62 +89,69 @@ class IntCodeState:
     @property
     def base(self):
         """Return relative base"""
-        return self._relative_base
+        return self._base
 
-    def read(self, offset=0, addr=None):
-        """Return value from memory"""
-        ptr = addr if addr != None else self._idx + offset
-        assert ptr >= 0, f"Invalid read(offset={offset}) from: {ptr}"
-        if ptr >= len(self.prog):
-            return 0
-        return self.prog[ptr]
-
-    def writ(self, value, offset=0):
-        """Write value to memory, extend memory if no space"""
-        ptr = self.read(offset)
-        if ptr >= len(self.prog):
-            self.prog.extend([0] * ((ptr + 1) - len(self.prog)))
-        if self._dbg:
-            print(f"writ({value},offset={offset}) -> self.prog[{ptr}]")
-        self.prog[ptr] = value
-
-    def next(self, offset=0):
-        """Progress to next instruction"""
-        self._idx += offset
-
-    def rbse(self, offset=0):
-        """Set relative base"""
-        self._relative_base += offset
-
-    def jump(self, ptr):
-        """Jump to instruction"""
-        self._idx = ptr
-
+    @property
     def halt(self):
         """Return true if halted"""
         return self._halt
 
-    def hlcf(self):
-        """Halt and catch fire. Set halt() to True"""
-        self._halt = True
+    def read(self, addr):
+        """Return value from memory"""
+        assert addr >= 0, f"Invalid read(addr={addr})"
+        if addr >= len(self.prog):
+            return 0
+        return self.prog[addr]
+
+    def writ(self, value, addr):
+        """Write value to memory, extend memory if no space"""
+        assert addr >= 0, f"Invalid writ(value={value}, addr={addr})"
+        if addr >= len(self.prog):
+            self.prog.extend([0] * ((addr + 1) - len(self.prog)))
+        self.prog[addr] = value
+
+    def next(self, offset):
+        """Progress to next instruction"""
+        self._idx += offset
+
+    def jump(self, addr):
+        """Jump to instruction"""
+        self._idx = addr
+
+    def rbse(self, offset):
+        """Set relative base"""
+        self._base += offset
 
     def inst(self):
-        """Return opcode and modes"""
-        code = self.read()
+        """Return opcode and modes from current position"""
+        code = self.read(self.idx)
         opcode = code % 100
         modes = [int(code/100)%10, int(code/1000)%10, int(code/10000)%10]
         return opcode, modes
 
+    def addr(self, offset, mode):
+        """Resolve memory address"""
+        value = self.read(self._idx + offset)
+        if mode == 0:       # position mode
+            return value
+        elif mode == 2:     # relative mode
+            return self._base + value
+        assert False, f"Invalid addr(offset={offset}, mode={mode})"
+
     def parm(self, offset, mode):
         """Read parameter based on mode"""
-        value = self.read(offset)
+        value = self.read(self._idx + offset)
         if mode == 0:       # position mode
-            return self.read(addr=value)
+            return self.read(value)
         elif mode == 1:     # immediate mode
             return value
         elif mode == 2:     # relative mode
-            return self.read(addr=self._relative_base+value)
-        assert False, f"Failed with mode: {mode}"
+            return self.read(self._base + value)
+        assert False, f"Invalid parm(offset={offset}, mode={mode})"
+
+    def hlcf(self):
+        """Halt and catch fire. Set halt state to True"""
+        self._halt = True
 
 
 def null_debugger(*args):
@@ -129,9 +162,10 @@ def null_debugger(*args):
 def cmd_debugger(state, cmd, modes, args, result):
     """Dump processor instructions to stdout"""
     arglist = []
+    ptr = state.idx
     for i in range(args):
-        arglist.append(state.read(i+1))
-    res = state.read(args+1) if result else '<undef>'
+        arglist.append(state.read(ptr + i + 1))
+    res = state.read(ptr + args + 1) if result else '<undef>'
     print(f"{cmd}({arglist} m:{modes}) -> {res} [idx:{state.idx},base:{state.base}]")
 
 
@@ -141,14 +175,16 @@ def run(state, io, debug=null_debugger):
     def add(modes):
         """Addition operator: 01, arg, arg, result"""
         debug(state, 'add', modes, 2, True)
-        state.writ(state.parm(1, modes[0]) + state.parm(2, modes[1]), 3)
+        value = state.parm(1, modes[0]) + state.parm(2, modes[1])
+        state.writ(value, state.addr(3, modes[2]))
         state.next(4)
         return True
 
     def multiply(modes):
         """Multiplication operator: 02, arg, arg, result*"""
         debug(state, 'multiply', modes, 2, True)
-        state.writ(state.parm(1, modes[0]) * state.parm(2, modes[1]), 3)
+        value = state.parm(1, modes[0]) * state.parm(2, modes[1])
+        state.writ(value, state.addr(3, modes[2]))
         state.next(4)
         return True
 
@@ -156,7 +192,7 @@ def run(state, io, debug=null_debugger):
         """Take input operator: 03, arg <- read input to arg"""
         debug(state, 'read_input', modes, 0, True)
         if io.has_in():
-            state.writ(io.read_in(), offset=1)
+            state.writ(io.read_in(), state.addr(1, modes[0]))
             state.next(2)
             return True
         return False
@@ -189,20 +225,22 @@ def run(state, io, debug=null_debugger):
     def less_than(modes):
         """Less-than operator: 07, arg, arg, result"""
         debug(state, 'less_than', modes, 2, True)
+        addr = state.addr(3, modes[2])
         if state.parm(1, modes[0]) < state.parm(2, modes[1]):
-            state.writ(1, 3)
+            state.writ(1, addr)
         else:
-            state.writ(0, 3)
+            state.writ(0, addr)
         state.next(4)
         return True
 
     def equals(modes):
         """Less-than operator: 08, arg, arg, result"""
         debug(state, 'equals', modes, 2, True)
+        addr = state.addr(3, modes[2])
         if state.parm(1, modes[0]) == state.parm(2, modes[1]):
-            state.writ(1, 3)
+            state.writ(1, addr)
         else:
-            state.writ(0, 3)
+            state.writ(0, addr)
         state.next(4)
         return True
 
@@ -233,14 +271,14 @@ def run(state, io, debug=null_debugger):
         99: halt
     }
 
-    assert not state.halt()
+    assert not state.halt
     try:
         while True:
             code, modes = state.inst()
             opcode = operators.get(code)
             assert opcode, f"operation failed at {state.idx} -> {state.read()}"
             if not opcode(modes):
-                return state.halt()
+                return state.halt
 
     except IndexError:
         assert False, f"Invalid index: {state.idx}"
