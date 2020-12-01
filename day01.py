a4fa929f180e71e26cce9c7b4315ac010ccc997e
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2020-12-01 08:58:06 +0200

    Solve part 2 of day 1 of 2020
    The puzzle solving algorithm is bit rude, but works.

--- day01.py
+++ day01.py
@@ -4,12 +4,28 @@ import sys
 
 
 def solve_part1(input):
-    """Solve with set and iteration"""
+    """Solve with set and iteration.
+
+    Value 600 is not handled correctly
+    """
     data = set(input)
     for v1 in input:
         v2 = 2020 - v1
         if v2 in data:
-            return (v1*v2, v1, v2)
+            return (v1 * v2, v1, v2)
+
+
+def solve_part2(input):
+    """Solve with multiple iteration
+
+    Values where same value can be used multiple times in sum will give
+    wrong answers
+    """
+    for v1 in input:
+        for v2 in input:
+            v3 = 2020 - (v1 + v2)
+            if v3 in input:
+                return (v1 * v2 * v3, v1, v2, v3)
 
 
 def read_data(file):
@@ -20,8 +36,10 @@ def read_data(file):
 def main():
     assert len(sys.argv) == 2, "Missing input"
     data = read_data(sys.argv[1])
-    result, value1, value2 = solve_part1(data)
-    print(f"Part 1: {value1} + {value2} = {value1+value2} => {value1} * {value2} = {result}")
+    result, v1, v2 = solve_part1(data)
+    print(f"Part 1: {v1} + {v2} = {v1+v2} => {v1} * {v2} = {result}")
+    result, v1, v2, v3 = solve_part2(data)
+    print(f"Part 1: {v1} + {v2} + {v3} = {v1+v2+v3} => {v1} * {v2} * {v3} = {result}")
 
 
 if __name__ == "__main__":
--- test_day01.py
+++ test_day01.py
@@ -6,3 +6,9 @@ def test_solution1():
     data = [1721, 979, 366, 299, 675, 1456]
     result, _, _ = day01.solve_part1(data)
     assert result == 514579
+
+
+def test_solution2():
+    data = [1721, 979, 366, 299, 675, 1456]
+    result, _, _, _ = day01.solve_part2(data)
+    assert result == 241861950
