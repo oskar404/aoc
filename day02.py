10ffa02bf73790dfbe1028ae2ecaf75199c16ca2
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2020-12-02 21:18:32 +0200

    Solve part 2 of day 2 of 2020
    Reading carefully the requirements will go far. Debugging the
    goes on until found out the the second part had wrong logic.

--- day02.py
+++ day02.py
@@ -4,7 +4,7 @@ import re
 import sys
 
 
-class PasswdRule:
+class OldPasswdRule:
     def __init__(self, min, max, char):
         self.min = min
         self.max = max
@@ -15,7 +15,19 @@ class PasswdRule:
         return c >= self.min and c <= self.max
 
 
-def parse_input(data):
+class TobogganRule:
+    def __init__(self, idx1, idx2, char):
+        self.idx1 = idx1
+        self.idx2 = idx2
+        self.char = char
+
+    def match(self, passwd):
+        c1 = passwd[self.idx1]
+        c2 = passwd[self.idx2]
+        return c1 != c2 and (c1 == self.char or c2 == self.char)
+
+
+def parse_input(data, rule):
     """Parses lines as password entries with rule"""
     pattern = "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)"
     prog = re.compile(pattern)
@@ -23,22 +35,22 @@ def parse_input(data):
     for line in data:
         m = prog.match(line)
         min, max, char, passwd = m.groups()
-        result.append((PasswdRule(int(min), int(max), char), passwd))
+        result.append((rule(int(min) - 1, int(max) - 1, char), passwd))
     return result
 
 
 def solve_part1(input):
-    """Find number of entries matching the password requirements"""
-    passwords = parse_input(input)
-    found = 0
-    for entry in passwords:
-        if entry[0].match(entry[1]):
-            found += 1
-    return found
+    """Find matching the passwords using OldPasswdRule"""
+    passwords = parse_input(input, OldPasswdRule)
+    matches = [rule.match(passwd) for rule, passwd in passwords]
+    return sum(matches)
 
 
 def solve_part2(input):
-    pass
+    """Find matching the passwords requirements using TobogganRule"""
+    passwords = parse_input(input, TobogganRule)
+    matches = [rule.match(passwd) for rule, passwd in passwords]
+    return sum(matches)
 
 
 def read_data(file):
@@ -51,6 +63,8 @@ def main():
     data = read_data(sys.argv[1])
     result = solve_part1(data)
     print(f"Part 1: valid passwords {result}")
+    result = solve_part2(data)
+    print(f"Part 2: valid passwords {result}")
 
 
 if __name__ == "__main__":
