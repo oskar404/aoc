aec78564e2f5c13284b2ee866fd4b045ddf3f533
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2020-12-13 20:13:49 +0200

    Part 2 of day 13 year 2020
    Had to fall back to reddit to figure out the solution. It seems
    that need study more math and Chinese Remainder Theorem.

--- day13.py
+++ day13.py
@@ -27,8 +27,27 @@ def solve_part1(input, verbose=False):
     return (code, buses[idx], next_bus - timestamp)
 
 
+def contest(input):
+    """Return list of bus id, offset tuples"""
+    rows = [l.strip() for l in input.splitlines() if l.strip()]
+    return [(int(b), i) for i, b in enumerate(rows[1].split(",")) if b != "x"]
+
+
 def solve_part2(input, verbose=False):
-    pass
+    # Solution more or less copied from reddit ..
+    # https://www.reddit.com/r/adventofcode/comments/kc4njx/2020_day_13_solutions/gfpeb1k?utm_source=share&utm_medium=web2x&context=3
+
+    data = contest(input)
+    if verbose:
+        print(f"rules: {data}")
+
+    jump = candidate = data[0][0]
+    for bus in data[1:]:
+        while (candidate + bus[1]) % bus[0] != 0:
+            candidate += jump
+        jump *= bus[0]
+
+    return candidate
 
 
 def read_data(file):
@@ -41,6 +60,7 @@ def main():
     data = read_data(sys.argv[1])
     code, bus, wait = solve_part1(data)
     print(f"Part 1: code:{code} bus:{bus} wait:{wait}")
+    print(f"Part 2: solution:{solve_part2(data)}")
 
 
 if __name__ == "__main__":
--- test_day13.py
+++ test_day13.py
@@ -15,5 +15,36 @@ def test_solution1():
     assert wait == 5
 
 
+data1 = """
+XXX
+17,x,13,19
+"""
+
+data2 = """
+XXX
+67,7,59,61
+"""
+
+data3 = """
+XXX
+67,x,7,59,61
+"""
+
+data4 = """
+XXX
+67,7,x,59,61
+"""
+
+data5 = """
+XXX
+1789,37,47,1889
+"""
+
+
 def test_solution2():
-    pass
+    assert day13.solve_part2(data1, True) == 3417
+    assert day13.solve_part2(data2, True) == 754018
+    assert day13.solve_part2(data3, True) == 779210
+    assert day13.solve_part2(data4, True) == 1261476
+    assert day13.solve_part2(data5, True) == 1202161486
+    assert day13.solve_part2(input, True) == 1068781
