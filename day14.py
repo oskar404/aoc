fc6b0079e3bf7ac13d91c54fc1917ca46d922a3c
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2020-12-19 18:14:20 +0200

    Solve part 2 of day 14 year 2020
    More serious bitmask handling.

--- day14.py
+++ day14.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 
 
-def parse(input):
+def parse1(input):
     """Parse input data and return as list of tuples"""
 
     def splitter(value):
@@ -19,25 +20,27 @@ def parse(input):
     return rows
 
 
-ops = {
-    "0": lambda arg, shift: 0,
-    "1": lambda arg, shift: 1 << shift,
-    "X": lambda arg, shift: arg & (1 << shift),
+ops_v1 = {
+    "0": lambda arg, bit: 0,
+    "1": lambda arg, bit: 1 << bit,
+    "X": lambda arg, bit: arg & (1 << bit),
 }
 
 
 def solve_part1(input, verbose=False):
-    """Return code is sum of all memory addresses"""
-    data = parse(input)
+    """Return code is sum of all memory addresses
+
+    version 1 decoder chip
+    """
+    data = parse1(input)
     mem = {}
 
     if verbose:
-        print("DATA: ***")
+        print("PROG: ***")
         for r in data:
             print(r)
 
     mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-
     for e in data:
         if e[0] == "mask":
             mask = e[1]
@@ -45,7 +48,7 @@ def solve_part1(input, verbose=False):
         else:
             v = 0
             for pos in range(36):
-                v += ops[mask[35 - pos]](e[1], pos)
+                v += ops_v1[mask[35 - pos]](e[1], pos)
             mem[e[0]] = v
 
     if verbose:
@@ -56,8 +59,81 @@ def solve_part1(input, verbose=False):
     return sum(mem.values())
 
 
+def parse2(input):
+    """Parse input data and return as list of tuples"""
+    pattern = r"mem\[([0-9]+)\]"
+    matcher = re.compile(pattern)
+
+    def splitter(value):
+        k, v = value.split("=")
+        k = k.strip()
+        if k != "mask":
+            v = int(v)
+            k = int(matcher.match(k).group(1))
+        else:
+            v = v.strip()
+        return (k, v)
+
+    rows = [splitter(l) for l in input.splitlines() if l.strip()]
+    return rows
+
+
+def set_bit(addrs, bit):
+    """Set address bit to one"""
+    result = []
+    for addr in addrs:
+        result.append(addr | (1 << bit))
+    return result
+
+
+def duplicate_bit(addrs, bit):
+    """Set address bit to zero and one i.e. duplicate adderesses"""
+    id_mask = 0b111111111111111111111111111111111111
+    result = []
+    for addr in addrs:
+        result.append(addr | (1 << bit))
+        result.append(addr & ((1 << bit) ^ id_mask))
+    return result
+
+
+ops_v2 = {
+    "0": lambda addrs, bit: addrs,
+    "1": set_bit,
+    "X": duplicate_bit,
+}
+
+
 def solve_part2(input, verbose=False):
-    pass
+    """Return code is sum of all memory addresses
+
+    version 2 decoder chip
+    """
+    data = parse2(input)
+    mem = {}
+
+    if verbose:
+        print("PROG: ***")
+        for r in data:
+            print(r)
+
+    mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    for e in data:
+        if e[0] == "mask":
+            mask = e[1]
+            assert len(mask) == 36
+        else:
+            addrs = [e[0]]
+            for pos in range(36):
+                addrs = ops_v2[mask[35 - pos]](addrs, pos)
+            for addr in addrs:
+                mem[addr] = e[1]
+
+    if verbose:
+        print("MEM: ***")
+        for k in sorted(mem.keys()):
+            print(f"{k} -> {mem[k]}")
+
+    return sum(mem.values())
 
 
 def read_data(file):
@@ -68,7 +144,8 @@ def read_data(file):
 def main():
     assert len(sys.argv) == 2, "Missing input"
     data = read_data(sys.argv[1])
-    print(f"Part 1: code:{solve_part1(data)}")
+    print(f"Part 1: code: {solve_part1(data)}")
+    print(f"Part 2: code: {solve_part2(data)}")
 
 
 if __name__ == "__main__":
--- test_day14.py
+++ test_day14.py
@@ -2,7 +2,7 @@ import pytest
 import day14
 
 
-input = """
+input1 = """
     mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
     mem[8] = 11
     mem[7] = 101
@@ -11,9 +11,18 @@ input = """
 
 
 def test_solution1():
-    code = day14.solve_part1(input, True)
+    code = day14.solve_part1(input1, True)
     assert code == 165
 
 
+input2 = """
+    mask = 000000000000000000000000000000X1001X
+    mem[42] = 100
+    mask = 00000000000000000000000000000000X0XX
+    mem[26] = 1
+"""
+
+
 def test_solution2():
-    pass
+    code = day14.solve_part2(input2, True)
+    assert code == 208
